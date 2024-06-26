8aa7322eb0a4e2d1daab4edd51ae4d243c5a81aa
Tom J <tomj@Bitk-Air-2.lan>
2024-06-26 23:19:20 +0100

    Add CORS configuration and desktop shortcut for IPFS Web UI in setup_bkad.sh

--- scripts/setup_bkad.sh
+++ scripts/setup_bkad.sh
@@ -12,6 +12,8 @@ IPFS_PATH="/home/$IPFS_USER/.ipfs"
 IPFS_SERVICE="/etc/systemd/system/ipfs.service"
 AUTOSTART_DIR="/home/$IPFS_USER/.config/lxsession/LXDE-pi"
 AUTOSTART_FILE="$AUTOSTART_DIR/autostart"
+DESKTOP_DIR="/home/$IPFS_USER/Desktop"
+WEBUI_SHORTCUT="$DESKTOP_DIR/ipfs_webui.desktop"
 
 # Install prerequisites
 apt-get update
@@ -27,168 +29,12 @@ cd go-ipfs
 sudo -u $IPFS_USER ipfs init
 
 # Update IPFS configuration
-cat <<EOT > $IPFS_PATH/config
-{
-  "Identity": {
-    "PeerID": "12D3KooWSMBFo3UnbjmyfvyjzbAhr2NDrBRxgfdHM4gqsvpE7ZpC",
-    "PrivKey": "CAESQO9mCSd5yBucQpBJuwKw5IWuY3qfR1Y73IxXhArK9blE9Z+oGAWuLeXC0FEw24hM/3qrChnbqrE+meIqldRquJE="
-  },
-  "Datastore": {
-    "StorageMax": "10GB",
-    "StorageGCWatermark": 90,
-    "GCPeriod": "1h",
-    "Spec": {
-      "mounts": [
-        {
-          "child": {
-            "path": "blocks",
-            "shardFunc": "/repo/flatfs/shard/v1/next-to-last/2",
-            "sync": true,
-            "type": "flatfs"
-          },
-          "mountpoint": "/blocks",
-          "prefix": "flatfs.datastore",
-          "type": "measure"
-        },
-        {
-          "child": {
-            "compression": "none",
-            "path": "datastore",
-            "type": "levelds"
-          },
-          "mountpoint": "/",
-          "prefix": "leveldb.datastore",
-          "type": "measure"
-        }
-      ],
-      "type": "mount"
-    },
-    "HashOnRead": false,
-    "BloomFilterSize": 0
-  },
-  "Addresses": {
-    "Swarm": [
-      "/ip4/0.0.0.0/tcp/4001",
-      "/ip6/::/tcp/4001",
-      "/ip4/0.0.0.0/udp/4001/quic",
-      "/ip6/::/udp/4001/quic"
-    ],
-    "Announce": [],
-    "AppendAnnounce": [],
-    "NoAnnounce": [],
-    "API": "/ip4/0.0.0.0/tcp/5001",
-    "Gateway": "/ip4/0.0.0.0/tcp/8080"
-  },
-  "Mounts": {
-    "IPFS": "/ipfs",
-    "IPNS": "/ipns",
-    "FuseAllowOther": false
-  },
-  "Discovery": {
-    "MDNS": {
-      "Enabled": true,
-      "Interval": 10
-    }
-  },
-  "Routing": {
-    "Type": "dht"
-  },
-  "Ipns": {
-    "RepublishPeriod": "",
-    "RecordLifetime": "",
-    "ResolveCacheSize": 128
-  },
-  "Bootstrap": [
-    "/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
-    "/ip4/104.131.131.82/udp/4001/quic/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt"
-  ],
-  "Gateway": {
-    "HTTPHeaders": {
-      "Access-Control-Allow-Headers": [
-        "X-Requested-With",
-        "Range",
-        "User-Agent"
-      ],
-      "Access-Control-Allow-Methods": [
-        "GET"
-      ],
-      "Access-Control-Allow-Origin": [
-        "*"
-      ]
-    },
-    "RootRedirect": "",
-    "Writable": false,
-    "PathPrefixes": [],
-    "APICommands": [],
-    "NoFetch": false,
-    "NoDNSLink": false,
-    "PublicGateways": null
-  },
-  "API": {
-    "HTTPHeaders": {}
-  },
-  "Swarm": {
-    "AddrFilters": null,
-    "DisableBandwidthMetrics": false,
-    "DisableNatPortMap": false,
-    "RelayClient": {},
-    "RelayService": {},
-    "Transports": {
-      "Network": {},
-      "Security": {},
-      "Multiplexers": {}
-    },
-    "ConnMgr": {
-      "Type": "basic",
-      "LowWater": 600,
-      "HighWater": 900,
-      "GracePeriod": "20s"
-    }
-  },
-  "AutoNAT": {},
-  "Pubsub": {
-    "Router": "",
-    "DisableSigning": false
-  },
-  "Peering": {
-    "Peers": null
-  },
-  "DNS": {
-    "Resolvers": {}
-  },
-  "Migration": {
-    "DownloadSources": [],
-    "Keep": ""
-  },
-  "Provider": {
-    "Strategy": ""
-  },
-  "Reprovider": {
-    "Interval": "12h",
-    "Strategy": "all"
-  },
-  "Experimental": {
-    "FilestoreEnabled": false,
-    "UrlstoreEnabled": false,
-    "GraphsyncEnabled": false,
-    "Libp2pStreamMounting": false,
-    "P2pHttpProxy": false,
-    "StrategicProviding": false,
-    "AcceleratedDHTClient": false
-  },
-  "Plugins": {
-    "Plugins": null
-  },
-  "Pinning": {
-    "RemoteServices": {}
-  },
-  "Internal": {}
-}
-EOT
+sudo -u $IPFS_USER jq '.Addresses.API = "/ip4/0.0.0.0/tcp/5001"' $IPFS_PATH/config > $IPFS_PATH/config.tmp && mv $IPFS_PATH/config.tmp $IPFS_PATH/config
+sudo -u $IPFS_USER jq '.Addresses.Gateway = "/ip4/0.0.0.0/tcp/8080"' $IPFS_PATH/config > $IPFS_PATH/config.tmp && mv $IPFS_PATH/config.tmp $IPFS_PATH/config
+sudo -u $IPFS_USER jq '.API.HTTPHeaders = {
+  "Access-Control-Allow-Origin": ["http://192.168.1.103:5001", "http://localhost:3000", "http://127.0.0.1:5001", "https://webui.ipfs.io"],
+  "Access-Control-Allow-Methods": ["PUT", "POST"]
+}' $IPFS_PATH/config > $IPFS_PATH/config.tmp && mv $IPFS_PATH/config.tmp $IPFS_PATH/config
 
 # Create a systemd service for IPFS
 cat <<EOT > $IPFS_SERVICE
@@ -218,6 +64,21 @@ chown -R $IPFS_USER:$IPFS_USER $AUTOSTART_DIR
 
 # Ensure the autostart file is executable
 chmod +x $AUTOSTART_FILE
+
+# Add shortcut to IPFS Web UI on desktop
+mkdir -p $DESKTOP_DIR
+cat <<EOT > $WEBUI_SHORTCUT
+[Desktop Entry]
+Name=IPFS Web UI
+Comment=Access the IPFS Web UI
+Exec=chromium-browser http://localhost:5001/webui
+Icon=web-browser
+Terminal=false
+Type=Application
+Categories=Network;WebBrowser;
+EOT
+chown -R $IPFS_USER:$IPFS_USER $WEBUI_SHORTCUT
+chmod +x $WEBUI_SHORTCUT
 
 echo "BKAD setup complete. Please reboot the system."
 
